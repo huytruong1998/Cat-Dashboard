3567488c6a07e627659152a4f2776925d30d28d9
Huy Truong <skyzerzlol@gmail.com>
2022-06-14 17:47:49 +0300

    feat: frontend react webpack setup

---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx", ".json"],
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.ts(x?)$/,
+        exclude: ["/node_modules/", "/src/scss/"],
+        use: ["babel-loader", "ts-loader"],
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"],
+      },
+    ],
+  },
+};
