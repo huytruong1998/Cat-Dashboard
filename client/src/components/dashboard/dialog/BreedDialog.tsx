7204156561078390b69785f936b31576f7b51bf8
Huy Truong <skyzerzlol@gmail.com>
2022-06-18 02:09:28 +0300

    feat: responsive dialog styling

--- client/src/components/dashboard/dialog/BreedDialog.tsx
+++ client/src/components/dashboard/dialog/BreedDialog.tsx
@@ -13,10 +13,10 @@ const BreedDialog: React.FC<BreedDialogProps> = ({ open, handleClose }) => {
     <Modal open={open} onClose={handleClose}>
       <Box className="container">
         <Grid container spacing={2}>
-          <Grid item xs={6}>
+          <Grid item xs={12} md={6}>
             <img style={{ width: "100%" }} src={defaultLogo} alt="" />
           </Grid>
-          <Grid style={{ gridRowGap: 10 }} container item xs={6}>
+          <Grid style={{ gridRowGap: 10 }} container item xs={12} md={6}>
             <TextField fullWidth label="Name" variant="outlined" />
             <TextField fullWidth label="Origin" variant="outlined" />
             <TextField fullWidth label="Image Url" variant="outlined" />
@@ -30,7 +30,7 @@ const BreedDialog: React.FC<BreedDialogProps> = ({ open, handleClose }) => {
               rows={4}
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item md={12}>
             <Button
               variant="contained"
               color="error"
